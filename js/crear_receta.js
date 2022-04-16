38888dacea774abf4ec182d7c34a7a0334a59c90
Serafín Piña <arespin23@gmail.com>
2022-04-16 14:23:07 +0200

    crear receta full

--- js/crear_receta.js
+++ js/crear_receta.js
@@ -37,7 +37,7 @@ function eventoBoton(e){
 }
 
 //Llena la tabla de alimentos
-function llenarTabla(d){
+function llenarTabla(d=[]){
     //limitador de elementos mostrados
     let elem = 10;
     let n= 0;
@@ -54,8 +54,7 @@ function llenarTabla(d){
         //td.setAttribute('id', d[i].id_producto)
         td.setAttribute('id', 'fila')
         td.innerHTML= d[i].nombre_p;
-        tr.appendChild(td);
-        
+        tr.appendChild(td);        
         
         td = document.createElement('td');
         let input = document.createElement('input');
@@ -64,8 +63,7 @@ function llenarTabla(d){
         input.setAttribute('value','0');
         input.setAttribute('min','0');
         td.appendChild(input);
-        tr.appendChild(td);
-        
+        tr.appendChild(td);        
        
         td = document.createElement('td');        
         
@@ -74,8 +72,7 @@ function llenarTabla(d){
         img.addEventListener('click', addFila,false);
 
         td.appendChild(img);
-        tr.appendChild(td);
-        
+        tr.appendChild(td);        
       
         n++;
         tbody.appendChild(tr);
@@ -98,7 +95,7 @@ function addFila(img){
     let producto= img.target.parentNode.previousSibling.previousSibling.textContent;
     //capturar la fila hermana, que contiene la cantidad del producto
     let cantidad= img.target.parentNode.previousSibling.lastChild.value;
-    //console.log(cantidad);
+   
     let p = new Array(id_producto,producto,cantidad);
     //Agregar la fila al listado de recetas en un array
     arrayProductos.push(p);
@@ -107,11 +104,11 @@ function addFila(img){
 
 let btnFinalizar = document.getElementById('finalizar');
 btnFinalizar.addEventListener('click', finalizarReceta);
+let ul = document.getElementById('ingredientes');
 function agregaIngrediente(p){
     
     btnFinalizar.hidden = false;
     
-    let ul = document.getElementById('ingredientes');
     let elem = document.createElement('li');
     elem.textContent= p[1] + ": " + p[2];
 
@@ -127,10 +124,10 @@ formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
     form = new FormData(formulario);
     myReceta= form.get('receta');
-    //console.log(form.get('receta'));
+   
     //URL de la peticion
     let url= "./controller/guardar_receta.php";
-    //let d;   
+     
     //configurar la peticion. AQUI CONFIGURO LA PETICION
     let configFetch={
         method:'POST',
@@ -145,8 +142,7 @@ formulario.addEventListener('submit', (e)=>{
     promesa.then( res => res.json())
         .then(datos => {         
             let d = datos;
-            //llenarTabla(d)     
-            //console.log(d.error); 
+           
             mostarMensaje(d)          
      });
 })
@@ -180,7 +176,7 @@ function mostarMensaje(msj){
 function finalizarReceta(){
     //Nombre de la receta
     let r = myReceta;
-    //console.log(r);
+   
     //OBtener el id de la receta
     let url= "./controller/lista_recetas.php";       
     //configurar la peticion. AQUI CONFIGURO LA PETICION
@@ -195,9 +191,8 @@ function finalizarReceta(){
         .then( res => res.json())
         .then(datos => {         
             d = datos;
-            //en d[0].id_recetas está la id  
-            //guardadoFinalizarReceta(d[0].id_recetas)
-            document.cookie= "rece=" + d[0].id_recetas;
+           
+            document.cookie= "rece=" + d[0].id_recetas +"; Secure; SameSite=None";
         });
 
         //recuperar valor de la cookie
@@ -208,10 +203,9 @@ function finalizarReceta(){
     console.log(arrayProductos);
     
     url= "./controller/graba_receta_producto.php"; 
+    let codigo=0;
     for (let i = 0; i < arrayProductos.length; i++) {
-        //for (let j = 0; j < arrayProductos[i].length; j++)
-    // //     console.log(d);
-    // //     //Grabar datos en la tabla recetas_productos
+    
         configFetch={
             method:'POST',
             body:`receta=${cook[1]}&id_p=${arrayProductos[i][0]}&cantidad=${arrayProductos[i][2]}`,       
@@ -220,12 +214,27 @@ function finalizarReceta(){
         fetch(url,configFetch)
             .then( res => res.json())
             .then(datos => {         
-                console.log(datos);            
+                console.log(datos);  
+                codigo = datos.codigo; 
+                
+                if(codigo == 1){
+                    Swal.fire(
+                        "Receta creada con éxito",
+                        '',            
+                        'success'
+                    )
+            
+                    //si la receta se creado bien
+                    //reiniciar formulario
+                    ul.innerHTML ="";
+                    document.getElementById('finalizar').hidden = true;
+                    document.getElementById('frm').reset();
+                    document.getElementById('btn-buscador').disabled=true;
+                    document.getElementById('receta').disabled=false;
+                    llenarTabla();
+                }                
         });
-
-    // //     //console.log(arrayProductos[i][0] + "-" + arrayProductos[i][1] + "-" + arrayProductos[i][2]);
-        
-    }
+    }    
 }
 
 
